53e8333e03ceb7ee40cd9e81d1e4ceb199e78b3b
clikkle <140992107+clikkle@users.noreply.github.com>
2025-05-05 17:54:15 -0400

    Delete config directory

--- config/config.js
+++
@@ -1,8 +0,0 @@
-import './env.js';
-import './prototypes.js';
-import { connectDB } from './database.js';
-// import './watchers/uploads.js';
-
-if (process.env.NODE_ENV !== 'test') {
-    await connectDB('hr_clikkle');
-}
