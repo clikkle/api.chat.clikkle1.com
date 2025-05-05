2287eac315acf35c4269a698e9a486960df26fbb
clikkle <140992107+clikkle@users.noreply.github.com>
2025-05-05 17:57:36 -0400

    Delete schema directory

--- schema/Department.js
+++
@@ -1,8 +0,0 @@
-import { Schema, model , Types } from 'mongoose';
-
-const schema = new Schema({
-    adminId: { type: Types.ObjectId, required: true },
-    name: { type: String, required: true },
-});
-
-export default model('department', schema);
--- schema/unCompiled/CustomDate.js
+++
@@ -1,7 +0,0 @@
-import { Schema } from 'mongoose';
-
-export default new Schema({
-    year: { type: Number, required: true },
-    month: { type: Number, required: true },
-    day: { type: Number, required: true },
-});
